356149f600a54a880876019c158a5e43582df30d
Konstantinos Anastasiadis <skafosbiz@gmail.com>
2021-04-05 13:12:40 +0300

    Update paypal.js

--- Backend/config/paypal.js
+++ Backend/config/paypal.js
@@ -2,8 +2,8 @@ var paypal = require('paypal-rest-sdk');
 
 paypal.configure({
   mode: 'sandbox', // Sandbox or live
-  client_id: 'AaBfq1QGeV3zGfFVl6xnFh6QxTCRpgosC8Lmg__a2n1t39sr7oGgktQWmI7lLW4EaoOnWRBgnqhRTrh7',
-  client_secret: 'EK6VrjifHcbhtQeskfMHuKWB77rHQ2YlluJwIs6n6SlpsIdLCyEA_aUbtIXDMxnyzHAj4KIAAmlKjq0F'});
+  client_id: <YOUR CLIENT ID>,
+  client_secret: <YOUR SECRET ID>});
   
   
   
